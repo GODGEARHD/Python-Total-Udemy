6b46581b7e13c905260080b404e67b598a94f15e
GODGEAR HD <godgearhd22@gmail.com>
2022-08-18 10:48:06 +0200

    Python Total Udemy

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,19 +1,3 @@
 palabra1 = input("¿Película favorita?\n")
 palabra2 = input("¿Comida favorita?\n")
 print('El nombre de la cerveza es:\n "' + palabra1 + ' ' + palabra2 + '"')
-
-lista = ["a", "b", "c"]
-
-pos1 = lista[0]
-pos2 = lista[1]
-pos3 = lista[2]
-
-print(type(pos1))
-
-counter1 = palabra2.count(pos1)
-counter2 = palabra2.count(pos2)
-counter3 = palabra2.count(pos3)
-
-print(counter1)
-print(counter2)
-print(counter3)
